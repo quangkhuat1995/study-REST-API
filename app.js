2f63d2dfc4a51c2c516f02439a649f8181ce6e5c
Quang Khuat <quangkhuat1995@gmail.com>
2021-05-14 18:29:53 +0700

    demo

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 const feedRouter = require("./routes/feed");
-app.use(express.urlencoded({ extended: false }));
+
+// app.use(express.urlencoded({ extended: false })); // <form> x-www-form-urlencoded
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, PATCH, POST, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/feed", feedRouter);
-app.listen(8081);
+app.listen(5050);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,13 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "First title", content: "The content is here" }],
+  });
+};
+
+exports.createPosts = (req, res, next) => {
+  const { title, content } = req.body;
+  res.status(201).json({
+    message: "Post created successfully",
+    posts: [{ id: Date.now(), title, content }],
+  });
+};
